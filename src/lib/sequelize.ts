b97aecf47241b8cd75acb060378c41d93416104c
Wraitheon <tayyaabsajid@gmail.com>
2025-06-19 11:44:33 +0500

    Reset main to clean state

--- src/lib/sequelize.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-import config from '../config';
-
-const sequelize = new Sequelize(config.databaseUrl, {
-  dialect: 'postgres',
-  logging: false,
-});
-
-export default sequelize;
